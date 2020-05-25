93321ad2a3c67dc72d7391b97f80ee9313f630f7
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-05-25 11:01:11 +0100

    Update SELECT_from_Nobel.sql

--- SELECT_from_Nobel.sql
+++ SELECT_from_Nobel.sql
@@ -5,3 +5,32 @@
 SELECT yr, subject, winner
   FROM nobel
  WHERE yr = 1950
+ 
+
+-- Show who won the 1962 prize for Literature.
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+   
+
+-- Show the year and subject that won 'Albert Einstein' his prize.
+
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
+
+-- Give the name of the 'Peace' winners since the year 2000, including 2000.
+
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000;
+
+
+-- Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject ='Literature'  AND yr >= 1980 AND yr <= 1989
+
