1b9a32f2d702eb7df2d9d6c2dde44600d4a3f3b8
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-05-25 09:15:19 +0100

    Create SELECT_from_WORLD.sql

---
+++ SELECT_from_WORLD.sql
@@ -0,0 +1,22 @@
+SELECT name, continent, population FROM world
+
+SELECT name FROM world
+WHERE population >= 200000000
+
+SELECT name, gdp/population
+FROM world
+WHERE population >= 200000000;
+
+
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America';
+
+
+SELECT name, population
+FROM world
+WHERE NAME IN('France','Germany','Italy');
+
+SELECT name
+FROM world
+WHERE name LIKE '%United%';
